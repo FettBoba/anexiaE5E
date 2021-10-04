83aa060d48391337e1d2900ff4e810db153ee18f
Boba Fett <h.schmaldienst@gmail.com>
2021-10-04 12:52:25 +0200

    Update E5Etest.js

--- E5Etest.js
+++ E5Etest.js
@@ -1,1 +1,8 @@
-# here goes the code
+# most simple test function for Anexia e5e
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
